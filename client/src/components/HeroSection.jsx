fdd4fc14f5762e1c3b6e98353a50993dc58700b2
LohithGowda12 <lohithib04@gmail.com>
2025-12-14 10:26:23 +0530

    .

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
   return (
     <div className="flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url('/backgroundImage.png')] bg-cover bg-center h-screen">
         <img src={assets.marvelLogo} alt="" className="max-h-11 lg:h-11 mt-20"/>
-        <h1 className='text-5x1 md:text-[70px] md:leading-18 font-semibold max-w-110'>Guardians <br />of the Galaxy</h1>
+        <h1 className='text-5x1 md:text-[70px] md:leading-18 font-semibold max-w-110'>Guardians <br />of Galaxy</h1>
 
         <div className="flex items-center gap-4 text-gray-300">
             <span>Action | Adventure | Sci-Fi</span>
